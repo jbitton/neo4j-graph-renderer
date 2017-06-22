fa3202d1ef4519afd70a176879a005c3c43ad7c6
Joanna Bitton <joanna.bitton@gmail.com>
2017-06-22 11:27:37 -0400

    major refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import Neo4jGraphRenderer from 'Neo4jGraphRenderer';
+
+const App = () => (
+  <div>
+    <Neo4jGraphRenderer url="http://localhost:7474" user="neo4j" password="password" query="MATCH (s:Service)-[r]-(e:Event) RETURN s,r,e"/>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
